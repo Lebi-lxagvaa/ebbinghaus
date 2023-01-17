62bd21340f6b2d808b2d2d621a36192b6fa91dd6
Lebi-lxagvaa <41711615+Lebi-lxagvaa@users.noreply.github.com>
2023-01-17 03:20:30 +0100

    Update ebbinghausIllusion.py

--- ebbinghausIllusion.py
+++ ebbinghausIllusion.py
@@ -1,36 +1,28 @@
-def ebbinghausIllusion(breite=600, hoehe=400, r_mitte=20,
-                 r_kleiner=10, r_grosser=30, anzahl_kleiner=10, anzahl_grosser=6,
-                 distanz_kleiner=40, distanz_grosser=70,
-                 Farbe_mitte='purple', farbe_umringende='black',
-                 graph_distanz=200):
+def ebbinghausIllusion(r_mitte=20, r_kleiner=10, r_grosser=30, anzahl_kleiner=10, 
+                 anzahl_grosser=6, distanz_kleiner=40, distanz_grosser=70,
+                 Farbe_mitte='red', farbe_umringende='black', graph_distanz=200):
     
     #Dependencies 
     import math
     import turtle
-    #from ipyturtle3 import hold_canvas
-    #myCanvas=turtle.Canvas(width=breite,height=hoehe)
-    #display(myCanvas)
-    #myTS=turtle.TurtleScreen(myCanvas)
-    #myTS.bgcolor("lightblue")
-    #sc = turtle.Screen()
-    #sc.setup(400,300)
-    myPen=turtle.Turtle()
+    
+    stift=turtle.Turtle()
     
     #Koordinaten von mittleren kreisen
     x1, y1 = -distanz_grosser, 0
     x2, y2 = x1 + graph_distanz, y1
     
-    def zeichneKreis(x, y, radius, farbe, myPen):
-        myPen.setheading(0)
-        myPen.penup()
-        myPen.color(farbe)
-        myPen.fillcolor(farbe)
-        myPen.goto(x,y-radius)
-        myPen.begin_fill()
-        myPen.circle(radius)
-        myPen.end_fill()
-        myPen.pendown()
-        myPen.speed(500)
+    def zeichneKreis(x, y, radius, farbe, zeichne):
+        stift.setheading(0)
+        stift.penup()
+        stift.color(farbe)
+        stift.fillcolor(farbe)
+        stift.goto(x,y-radius)
+        stift.begin_fill()
+        stift.circle(radius)
+        stift.end_fill()
+        stift.pendown()
+        stift.speed(500)
         
     def umringen(umring_radius, r, x0, y0, anzahl_kreise):
         
@@ -38,14 +30,9 @@ def ebbinghausIllusion(breite=600, hoehe=400, r_mitte=20,
             grad = 2 * math.pi * i / anzahl_kreise
             x = x0 + umring_radius * math.cos(grad)
             y = y0 + umring_radius * math.sin(grad)
-            zeichneKreis(x, y, r, farbe_umringende, myPen)
+            zeichneKreis(x, y, r, farbe_umringende, stift)
     
-    zeichneKreis(x1,y1,r_mitte,Farbe_mitte,myPen)
+    zeichneKreis(x1,y1,r_mitte,Farbe_mitte,stift)
     umringen(distanz_grosser, r_grosser, x1, y1,anzahl_grosser)
-    zeichneKreis(x2,y2,r_mitte,Farbe_mitte,myPen)
+    zeichneKreis(x2,y2,r_mitte,Farbe_mitte,stift)
     umringen(distanz_kleiner, r_kleiner, x2, y2, anzahl_kleiner)
-    
-    
-ebbinghausIllusion(r_kleiner=10, r_grosser=30, anzahl_kleiner=10, anzahl_grosser=6,
-                 distanz_kleiner=40, distanz_grosser=70,
-                 Farbe_mitte='red', farbe_umringende='black')
